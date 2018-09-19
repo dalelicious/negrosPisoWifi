1719d29f30d3e0e544f5262b63a8f23b6886562e
demrock <denmarkcontrevida@gmail.com>
2018-09-20 00:54:32 +0800

    Add keywords meta

--- home/contactUs.php
+++ home/contactUs.php
@@ -1,13 +1,8 @@
-
-
-	<div class="bg-header img-responsive" style="">
+<div class="bg-header img-responsive" style="">
 			<div class="col-md-12">
 				<div class="container">
 					<h2 class="text-white header-text-big"> Globe 09457243966 <br> Smart 09283216238 </h2> 
 					<h2 class=" text-white header-text-small"> Email: negrospisowifi@gmail.com <br> Facebook: @negrospisowifi </h2>
-					
-
-
 				</div>
 			</div>
 		</div>
