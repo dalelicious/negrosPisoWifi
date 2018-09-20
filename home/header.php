788cb79a8fc6addf90f91cf43cdef7478b68bcb3
demrock <denmarkcontrevida@gmail.com>
2018-09-20 15:30:01 +0800

    add ado

--- home/header.php
+++ home/header.php
@@ -4,9 +4,11 @@
 			<div class="col-md-12">
 				<div class="container">
 					<h2 class=" text-white header-text-big">Start small. Earn Big. Start your business now!</h2>  
-					<h6 class=" text-white header-text-small">Official Distributor of Ado Piso Wifi</h6> 
+					<h6 class=" text-white header-text-small">Strictly for Negros Occidental.</h6> 
 					<a class="btn btn-primary" href="../home/?view=reseller" style="">Buy now</a>
-					<a class="btn btn-primary" href="../home/?view=contactUs" style=""> Become our Reseller</a>
+					<a class="btn btn-primary" href="../home/?view=contactUs" style=""> Become our Reseller</a> 
+					<h6 class="text-white header-text-small"> For inquiries outside Negros Occidental, </h6> 
+					<a class="btn btn-primary" href="https://adopisowifi.com/buy" target="_blank" style=""> Ado Distributors</a>
 				</div>
 			</div>
 		</div>
