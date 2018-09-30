bec95f031bbe7afa87d62fbd170885df110ea23c
acknolodgia <torredale1014@gmail.com>
2018-09-30 21:42:15 +0800

    Add meta head

--- include/_zero.php
+++ include/_zero.php
@@ -7,9 +7,14 @@ include_once($ROOT."config/Models.php");
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:url"             content="https://negrospisowifi.com" />
+    <meta property="og:type"            content="article" />
+    <meta property="og:title"           content="Start Small. Earn Big." />
+    <meta property="og:description"     content="Start your business with Wifi Vendo Machine" />
+    <meta property="og:image"           content="https://negrospisowifi/include/assests/img/logo.png">
+    <meta name="viewport"               content="width=device-width, initial-scale=1.0">
     <title>Negros Piso Wifi</title>
-		<link rel="icon" href="<?=$ROOT;?>include/assets/img/pisowifi.ico">
+    <link rel="icon" href="<?=$ROOT;?>include/assets/img/pisowifi.ico">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
