17051c5d36885bd57b9af6d97f00102e99fca5ef
acknolodgia <torredale1014@gmail.com>
2018-09-30 21:34:30 +0800

    Fix contact us process. add new page for success message.

---
+++ pages/success.php
@@ -0,0 +1,28 @@
+<?php
+$ROOT = "../";
+
+include $ROOT."include/_header.php";
+?>
+
+<section class="heading-title">
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<h1>Message Sent</h1>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container-fluid">
+		</br>
+		<p>Thank you for getting in touch!</br></br>
+
+		We have received your message and would like to thank you for writing to us. If your inquiry is urgent, please use the phone numbers 09457243966 or 09283216238 to talk to one of our staff members. Otherwise, we will reply by email or call you as soon as possible.</br></br>
+
+		Talk to you soon!</p>
+	</div>
+</section>
+
+<?php include $ROOT."include/_footer.php";?>
